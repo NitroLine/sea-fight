0b2227eaff077a286a3bbad1dd8012edf5043c0b
NitroLine <48989608+NitroLine@users.noreply.github.com>
2021-05-04 21:51:04 +0500

    add settings file

---
+++ settings.py
@@ -0,0 +1,23 @@
+WIDTH = 1080
+HEIGHT = 720
+FPS = 25
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+YELLOW = (60, 100, 100)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+LIGHT_RED = (200, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (150, 150, 255)
+ORANGE = (255,180,0)
+GRAY = (100,100,100)
+
+
+
+
+BUTTON_STYLE = {"hover_color" : LIGHT_BLUE,
+                "clicked_color" : WHITE,
+                "clicked_font_color" : BLACK,
+                "hover_font_color" : BLACK}
