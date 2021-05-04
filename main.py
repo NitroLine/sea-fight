72fca215d759973450420db3679d7b7339bc1543
NitroLine <48989608+NitroLine@users.noreply.github.com>
2021-05-04 21:50:26 +0500

    add settings file / update view for field

--- main.py
+++ main.py
@@ -4,28 +4,8 @@ from models.game import Game
 from views.button import Button
 from views.field import FieldView
 from views.text import Text
+from settings import *
 import random
-
-WIDTH = 1080
-HEIGHT = 720
-FPS = 25
-
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-LIGHT_BLUE = (150, 150, 255)
-ORANGE = (255,180,0)
-
-
-
-
-BUTTON_STYLE = {"hover_color" : LIGHT_BLUE,
-                "clicked_color" : WHITE,
-                "clicked_font_color" : BLACK,
-                "hover_font_color" : BLACK}
 
 # Создаем игру и окно
 pygame.init()
--- views/field.py
+++ views/field.py
@@ -1,5 +1,7 @@
-from pygame import Rect, draw, USEREVENT
+from pygame import Rect, draw
+import pygame
 from .base_view import BaseView
+from settings import *
 
 class FieldView(BaseView):
     def __init__(self, x, y, width, height):
@@ -9,11 +11,12 @@ class FieldView(BaseView):
         self.rect = Rect(x, y, width, height)
         self.rects = None
         self.fog_of_war = False
+        self.now_selected_ship = None
 
     def setup(self, field, fog_of_war):
         self.fog_of_war = fog_of_war
         self.field = field
-        self.rects = self.generate_point_to_rect(self.width, self.height)
+        self.rects = self.generate_point_to_rect(self.rect.width, self.rect.height)
 
     def change_size(self,width,height):
         self.rect.width = width
@@ -32,10 +35,59 @@ class FieldView(BaseView):
                 res[(self.x+x, self.y+y)] = rectangle
         return res
 
+
+    def draw_ship(self, surface,ship,conflicted_points,shots,use_light):
+        for point in ship.get_position_points():
+            if not self.fog_of_war or point in shots:
+                Drawer.draw_ship_cell(surface,self.rects[point],
+                                      use_light=use_light,
+                                      in_conflict= point in conflicted_points,
+                                      is_shot= point in shots)
+
+
     def update(self, surface):
         for rect in self.rects.values():
-            draw.rect(surface, [255, 0, 0], rect, 2)
+            Drawer.draw_cell(surface,rect)
+        conflicted_points = self.field.get_conflicted_points()
+        shots = self.field.get_shoots()
+        for shot in shots:
+            Drawer.draw_shot_cell(surface,self.rects[shot])
+        for ship in filter(lambda x: x != self.now_selected_ship, self.field.get_ships()):
+            self.draw_ship(surface,ship,conflicted_points,shots,False)
+        if self.now_selected_ship is not None:
+            self.draw_ship(surface,self.now_selected_ship,conflicted_points,shots,True)
+
+
 
     def check_event(self,event):
-        if event.type == USEREVENT:
+        if event.type == pygame.USEREVENT:
             print(event.data)
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            for point in self.rects:
+                if self.rects[point].collidepoint(pos):
+                    print(pos, self.rects[point])
+
+
+
+class Drawer:
+    @staticmethod
+    def draw_shot_cell(surface, rect):
+        draw.ellipse(surface,LIGHT_BLUE,rect)
+
+    @staticmethod
+    def draw_ship_cell(surface,rect,is_shot,in_conflict,use_light):
+        border_color = YELLOW if use_light else LIGHT_BLUE
+        color = LIGHT_RED if in_conflict else BLUE
+        draw.rect(surface, rect, color)
+        draw.rect(surface, rect, border_color, 2)
+        if is_shot:
+            draw.line(surface, GRAY, rect.topleft, rect.bottomright)
+            draw.line(surface, GRAY, rect.topright, rect.bottomleft)
+
+    @staticmethod
+    def draw_cell(surface, rect):
+        draw.rect(surface, GRAY, rect)
+        draw.rect(surface, LIGHT_BLUE, rect, 2)
+
+
